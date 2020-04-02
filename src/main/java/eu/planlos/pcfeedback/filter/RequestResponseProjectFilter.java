7abbc6d03f53fd052f7870f360f5814d56d406f3
derBobby <dev@planlos.eu>
2020-04-03 01:31:11 +0200

    Debug Rückbau

--- src/main/java/eu/planlos/pcfeedback/filter/RequestResponseProjectFilter.java
+++ src/main/java/eu/planlos/pcfeedback/filter/RequestResponseProjectFilter.java
@@ -1,7 +1,6 @@
 package eu.planlos.pcfeedback.filter;
 
 import java.io.IOException;
-import java.util.Enumeration;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -39,16 +38,6 @@ public class RequestResponseProjectFilter implements Filter {
 		HttpSession session = req.getSession();
 		Project project = (Project) session.getAttribute(SessionAttributeHelper.PROJECT);
 		
-		/** TODO remove DEBUG **/
-		Enumeration<String> attrs = session.getAttributeNames();
-		while(attrs.hasMoreElements()) {
-			String e = attrs.nextElement();
-			System.err.println(e);
-			Object o = session.getAttribute(e);
-			System.err.println(o.hashCode());
-		}
-		/** remove DEBUG **/
-		
 		// Check if session has project
 		if(project == null) {
 			LOG.error("No project saved in session -> redirecting do start page");
@@ -57,8 +46,6 @@ public class RequestResponseProjectFilter implements Filter {
 			String projectName = project.getName();
 			if(! ps.exists(projectName)) {
 				LOG.error("Project name='{}' saved in session does not exist -> sending 400", projectName);
-				//TODO uncomment res.sendError(HttpStatus.NOT_FOUND.value(), HttpStatus.NOT_FOUND.getReasonPhrase());
-				chain.doFilter(request, response); //TODO REMOVE
 			} else {
 				LOG.debug("Valid project saved in session: project={}", projectName);
 				chain.doFilter(request, response);
